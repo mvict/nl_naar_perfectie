dbeb6c8d45008f73f5a673d0c42706848ae02da1
Victoria Avila Navarro <Victoria.AvilaNavarro@tomtom.com>
2019-04-04 10:25:17 +0200

    read file without pandas question forming in class Question correction of vuitroepen

--- learn_these_words.py
+++ learn_these_words.py
@@ -4,47 +4,83 @@
 import pandas as pd
 import nltk
 import argparse
+import codecs
+import random
 
 
-# parameters --n number of words to practice
-# category --c pos of the words to learn
-# type --t  exercise type
-# list --l list of words to practice
+class Question:
+    def __init__(self, record):
+        self._word = record[1]
+        self._meaning = record[3]
+        self._default_question = "Wat betekent {} >>>".format(record[1])
+    #
+    #     if record[2] == 'verb':
+    #         self.__verb_question()
+    #         self._is_regular = record[6]
+    #
+    #     if record[2] == 'noun':
+    #         self.__noun_question()
+    #
+    #     if record[2] == 'adjective':
+    #         self.__noun_question()
+    #
+    # def __verb_question(self):
+    #     question1 = "Wat betekenis de vervoeging van %s".format()
+    #     question2 = "welke "
+    #
+    # def __noun_question(self):
+    #     question1 = "meervoud:"
+    #     question2 = "lidwoord:"
+    #
+    # def __adjective_question(self):
+    #     question1 = "meervoud:"
+    #     question2 = "lidwoord:"
+
+    def question_answer(self):
+        question = self._default_question
+        answer = self._meaning
+        return question, answer
+
+
+def build_exercise(number_of_tries, vocabulary_file, category):
+        with codecs.open(vocabulary_file, 'r','UTF-8') as input_file:
+            complete_vocabulary = [row.rstrip('\r\n').split('\t') for row in input_file]
+            if category == "":
+                main_routine(number_of_tries, complete_vocabulary)
+            else:
+                complete_set = [row for row in complete_vocabulary if row[2] == category]
+                print(complete_set)
+
+                random.shuffle(complete_set) #[:number_of_tries]
+                print(complete_set)
+
+                while number_of_tries:
+                    q = Question(complete_set[number_of_tries])
+                    question, answer = q.question_answer()
+                    user_answer = input(question)
+                    if user_answer == answer:
+                        print ("so far so good")
+                    else:
+                        print("not yet there, it was {}".format(answer))
+                    number_of_tries -= 1
+
+        # complete_vocabulary = ["id", "string", "pos", "meaning", "english", "opposite", "regular", "reflexive",
+        #                                 "trenbaar", "gender", "preposition"]
+
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-n', dest='number_of_words', help='number of words to practice', type=int)
-    # parser.add_argument('-c', dest='category', help='Grammar category or part of speech')
+    parser.add_argument('-c', dest='category', help='Grammar category or part of speech')
     # parser.add_argument('-t', dest='type', help='Type of exercise')
     parser.add_argument('-l', dest='list', help='Name of the list to practice')
 
     args = parser.parse_args()
-    vocabulary = pd.read_csv(args.list, sep='\t')
-    # First line is always taken as column name and can be overwriten as:
-    vocabulary.columns = ["id","string","pos","meaning","english","opposite","regular","reflexive","trenbaar","gender","preposition"]
-    # print(read_file.head())
-#    print(read_file["id"][0])
-
-    good_answer = 0
-    bad_answer = 0
-    length = len(vocabulary) - 1
+    vocabulary_file_name = args.list
     number_of_tries = args.number_of_words
+    category = args.category
 
-    while number_of_tries > 0:
-        first_input = input('What is the meaning of %s >>> ' %vocabulary['string'][number_of_tries])
-        if first_input == vocabulary['meaning'][number_of_tries]:
-            print("Yes")
-            good_answer += 1
-        else:
-            print("No, burro, it's %s!!!" %vocabulary['meaning'][number_of_tries])
-            bad_answer += 1
-        number_of_tries -= 1
-    print("Número de aciertos %d" %number_of_tries)
-    print("Número de aciertos %d" %good_answer)
-    print("Número de fallos %d" %bad_answer)
-
-    
-
+    build_exercise(number_of_tries, vocabulary_file_name, category)
 
 if __name__ == "__main__":
-     main()
+    main()
