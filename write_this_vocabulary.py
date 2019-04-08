d209be4e6614daa92990bf039d95f24a761b88a9
Victoria Avila Navarro <Victoria.AvilaNavarro@tomtom.com>
2019-04-08 18:28:19 +0200

    printed result to a default file "new_vocabulary.txt"

--- write_this_vocabulary.py
+++ write_this_vocabulary.py
@@ -4,7 +4,7 @@
 # [0 "id", 1 "string", 2 "pos", 3 "meaning", 4 "english", 5 "opposite", 6 "regular", 7"reflexive",
 # 8"trenbaar", 9"gender", 10"preposition", 11 "vervoeging", 12 plural_form]
 
-# TODO PRINT THE RESULT TO A FILE
+import codecs
 
 class GiveMeYourWord:
     def __init__(self,number):
@@ -39,25 +39,34 @@ class GiveMeYourWord:
             declination = 'n/a'
 
         self._word_in_list = [self._number, self._word, self._category, self._meaning, self._english,
-                              "n/a", regular, reflexive, separable, "n/a", preposition, declination,
-                              "n/a"]
+                              "n/a", regular, reflexive, separable, "n/a", preposition, declination, "n/a"]
+
     def _fill_in_adjective(self):
         self._word_in_list = [self._number, self._word, self._category, self._meaning, self._english,
                               "n/a", "n/a", "n/a", "n/a", "n/a", "n/a", "n/a", "n/a"]
 
-    def print_to_file(self):
+    def list_to_write(self):
         print(self._word_in_list)
-
-
+        if input("This entry will be added to your vocabulary, ok? (antwoord ja or no) >>>  ") == "ja":
+            return self._word_in_list
+        else:
+            return []
 def main():
 
     number = input("How many words do you want to add to your list? >>> ")
     number_of_words = int(number)
 
-    while number_of_words:
-        word = GiveMeYourWord(number_of_words)
-        word.print_to_file()
-        number_of_words -= 1
+    with codecs.open("new_vocabulary_file.txt",'w','UTF-8') as input_file:
+        while number_of_words:
+            word = GiveMeYourWord(str(number_of_words))
+            to_write_in_file = word.list_to_write()
+            if not to_write_in_file:
+                print("Ok, I didn't get it, sorry, lets try again")
+                pass
+            else:
+                record = '\t'.join(to_write_in_file) + '\n'
+                input_file.write(record)
+                number_of_words -= 1
 
 
 if __name__ == "__main__":
