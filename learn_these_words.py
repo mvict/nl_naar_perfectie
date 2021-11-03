09bca374beef12b01b539a3d47c180627e11c0e5
mvict <mvictoria.avila@gmail.com>
2021-11-03 20:28:17 +0100

    strings as constants and improvement names

--- learn_these_words.py
+++ learn_these_words.py
@@ -11,45 +11,62 @@ import random
 global_failures_counter = 0
 global_success_counter = 0
 
-# complete_vocabulary = [0 "id", 1 "string", 2 "pos", 3 "meaning", 4 "english", 5 "opposite", 6 "regular", 7"reflexive",
-#                                 8"trenbaar", 9"gender", 10"preposition", 11 "vervoeging", 12 plural_form]
+# complete_vocabulary = {0: "id", 1: "string", 2: "pos", 3: "meaning",
+#                        4: "english", 5: "opposite", 6: "regular",
+#                        7: "reflexivo, 8: "trenbaar", 9: "gender",
+#                        10: "preposition", 11:  "vervoeging",
+#                        12:  plural_form}
+
+ARTICLE = "Wat is het lidwoord van {} >>> "
+CORRECT_ANSWERS = "You answered {} questions correctly."
+HOWDOYOUSAY = "Hoe zegt je {} in Nederlands? >>> "
+MEANING = "Wat betekent {}? >>> "
+NUMBER_OF_QUESTIONS = "You got {} questions."
+PAST_PASTPARTICLE = "Wat is de verleden tijd en vooltooid deelwoord van {} >> "
+PLURAL = "Wat is de meervoud form van {} {} >>> "
+PREPOSITION = "Welk voorzetsel gebruik je met {} >>> "
+RESULT = "This is your result: "
+WRONG_ANSWERS = "You answered {} questions wrongly."
+WRONGNUMBEROFFIELDS = "Number of fields in line number {} is not as expected"
+
 
 class Question:
     def __init__(self, record):
         self._word = record[1]
+        self._category = record[2]
         self._meaning = record[3]
-        self._vragen = [["wat betekent {} >>> ".format(record[1]),record[3]],
-                        ["Hoe zeg je '{}' in Nederlands >>> ".format(record[3]),record[1]]]
+        self._vragen = [[MEANING.format(self._word), self._meaning],
+                        [HOWDOYOUSAY.format(self._meaning), self._word]]
 
-        if record[2] == 'verb':
+        if self._category == 'verb':
             self._is_regular = record[6]
             self._preposition = record[10]
             self._declination = record[11]
-            self.__verb_question()
+            self._verb_question()
 
-        if record[2] == 'noun':
+        if self._category == 'noun':
             self._plural = record[12]
             self._article = record[9]
-            self.__noun_question()
+            self._noun_question()
 
-    def __verb_question(self):
+    def _verb_question(self):
         # todo match boolean and not string
         if self._is_regular == 'FALSE':
-            question1 = "Wat is de verleden tijd en vooltooid deelwoord van {} >>> ".format(self._word)
+            question1 = PAST_PASTPARTICLE.format(self._word)
             answer1 = self._declination
             self._vragen.append([question1, answer1])
 
         if self._preposition != 'n/a':
-            question2 = "Welk voorzetsel gebruik je met {} >>> ".format(self._word)
+            question2 = PREPOSITION.format(self._word)
             answer2 = self._preposition
             self._vragen.append([question2, answer2])
 
-    def __noun_question(self):
-        question1 = "Wat is de meervoud form van {} {} >>> ".format(self._article,self._word)
+    def _noun_question(self):
+        question1 = PLURAL.format(self._article, self._word)
         answer1 = self._plural
         self._vragen.append([question1, answer1])
 
-        question2 = "Wat is het lidwoord van {} >>> ".format(self._word)
+        question2 = ARTICLE.format(self._word)
         answer2 = self._article
         self._vragen.append([question2, answer2])
 
@@ -59,6 +76,7 @@ class Question:
         question = chosen_question[0]
         answer = chosen_question[1]
         return question, answer
+
 
 def give_feedback_on_result(answer, user_answer):
     global global_failures_counter
@@ -82,45 +100,48 @@ def build_exercise(number_of_tries, vocabulary):
         number_of_tries -= 1
 
 
+# control function to check that the data file is correct
 def check_number_of_fields(vocabulary_file):
-        with codecs.open(vocabulary_file, 'r','UTF-8') as input_file:
-            line_number = 0
-            for row in input_file:
-                line_list = row.rstrip('\r\n').split('\t')
-                line_number += 1
-                try:
-                    assert(len(line_list) == 13)
-                except AssertionError:
-                    print ("Number of fields in line number {} is not as expected".format(line_number))
-        return True
+    with codecs.open(vocabulary_file, 'r', 'UTF-8') as input_file:
+        line_number = 0
+        for row in input_file:
+            line_list = row.rstrip('\r\n').split('\t')
+            line_number += 1
+            try:
+                assert(len(line_list) == 13)
+            except AssertionError:
+                print (WRONGNUMBEROFFIELDS.format(line_number))
+    return True
 
 
 def chose_vocabulary(file_name, category):
     with codecs.open(file_name, 'r', 'UTF-8') as input_file:
-        # complete vocabulary contains all records in file
-        complete_vocabulary = [row.rstrip('\r\n').split('\t') for row in input_file]
+        # all_vocabulary contains all records in file
+        all_vocabulary = [row.rstrip('\r\n').split('\t') for row in input_file]
         # if no category is given as argument
         if category is None:
-            return complete_vocabulary
+            return all_vocabulary
         else:
-            return [row for row in complete_vocabulary if row[2] == category]
+            return [row for row in all_vocabulary if row[2] == category]
 
 
 def summarize_performance(number_of_tries):
     print("\n############################################\n")
-    print("This is your result: ")
-    print("You got {} questions.".format(str(number_of_tries)))
-    print("You answered {} questions correctly.".format(str(global_success_counter)))
-    print("You answered {} questions wrongly.".format(str(global_failures_counter)))
+    print(RESULT)
+    print(NUMBER_OF_QUESTIONS.format(str(number_of_tries)))
+    print(CORRECT_ANSWERS.format(str(global_success_counter)))
+    print(WRONG_ANSWERS.format(str(global_failures_counter)))
     print("\n############################################\n")
 
 # todo move check number of fields to test file
 # todo change strings to ask question to constants, no magic questions.
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-n', dest='number_of_words', help='number of words to practice', type=int)
-    parser.add_argument('-c', dest='category', help='Grammar category or part of speech')
+    parser = argparse.ArgumentParser(description='Learn vocabulary lists')
+    parser.add_argument('-n', dest='number_of_words',
+                        help='number of words to practice', type=int)
+    parser.add_argument('-c', dest='category', help='Grammar category')
     # parser.add_argument('-t', dest='type', help='Type of exercise')
     parser.add_argument('-l', dest='list', help='Name of the list to practice')
 
@@ -132,9 +153,10 @@ def main():
 
     # check number of fields
     check_number_of_fields(vocabulary_file_name)
-    chosen_vocabulary = chose_vocabulary(vocabulary_file_name,category)
+    chosen_vocabulary = chose_vocabulary(vocabulary_file_name, category)
     build_exercise(number_of_tries, chosen_vocabulary)
     summarize_performance(number_of_tries)
+
 
 if __name__ == "__main__":
     main()
