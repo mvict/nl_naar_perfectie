4327c2d01a94fc7ca528173df1cbb75c5415aeaa
mvict <mvictoria.avila@gmail.com>
2021-11-03 21:22:46 +0100

    writing to history file improved

--- welk_lidwoord.py
+++ welk_lidwoord.py
@@ -3,39 +3,53 @@ import bs4
 import requests
 import sys
 
+SEARCHED_WORDS = []
 
-def look_up(word):
-    # look up gender in www.welklidwoord.nl
-    try:
-        url_gender_guesser = 'https://www.welklidwoord.nl/' + word
-        gender_guesser_page = requests.get(url_gender_guesser)
-        
-        if gender_guesser_page.status_code == requests.codes.ok:
-            beautifulsoup_of_genderpage = bs4.BeautifulSoup(gender_guesser_page.text, features='lxml')
-            gender = beautifulsoup_of_genderpage.select('h1 span')[0].getText()
+class Word:
+    def __init__(self, word):
+        self.word = word
 
-            if len(gender) > 3:
-                print(f"Sorry, ik weet het lidwoord van {word} niet")
-            # print gender to screen
-            else: 
-                print(f"Het lidwoord van {word} is \'{gender.lower()}\'")
+    def __str__(self):
+        return f"{self.word}"
+
+    def gender(self):
+        # look up gender in www.welklidwoord.nl
+        try:
+            url_gender_guesser = 'https://www.welklidwoord.nl/' + self.word
+            gender_guesser_page = requests.get(url_gender_guesser)
+            
+            if gender_guesser_page.status_code == requests.codes.ok:
+                beautifulsoup_of_genderpage = bs4.BeautifulSoup(gender_guesser_page.text, features='lxml')
+                gender = beautifulsoup_of_genderpage.select('h1 span')[0].getText()
+
+                if len(gender) > 3:
+                    return ""
+                else:
+                    return gender.lower()             
+        except(IndexError):
+            print("Please, write a complete word")
+
+def keep_track_of_search(gender, word):
+    SEARCHED_WORDS.append((gender, word))    
+
+def keep_search_history():
+    with open("searched_words.txt",'a') as output_file:
+        for gender, word  in SEARCHED_WORDS: 
+            output_file.write(gender + "\t" + word + "\n")
     
-    except(IndexError):
-        print("Please, write a complete word")
-
-# TODO: convert program to a class to be able to store 
-# the words with the gender
-
-def keep_track_of_search(word):
-    sw = open("saught_words.txt",'a')
-    sw.write(word + "\n")
-    sw.close()
-
 while(True):
     try:
         word = input("Welk woord zoek je? >>> ")
-        look_up(word)
-        keep_track_of_search(word)
+        w = Word(word)
+        g = w.gender()
+        if g:
+            print(f"Het lidwoord van {word} is \'{w.gender()}\'")
+            # adds tuple to SEARCHED_WORDS
+            keep_track_of_search(g, word)
+        else:
+            print(f"Ik weet niet welk lidwoord wordt gebruikt met {w}")
     except(KeyboardInterrupt):
+        # all searched words are written in once
+        keep_search_history()
         print("\n####### You have stopped the program, bye, bye") 
         sys.exit()
