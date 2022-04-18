31864e79c17229cd5e3861d95e3ae6900268cd99
mvict <mvictoria.avila@gmail.com>
2022-04-18 08:46:54 +0200

    refactor welk_lidwoord.py to keep track of searched words

--- welk_lidwoord.py
+++ welk_lidwoord.py
@@ -1,18 +1,39 @@
 # import webbrowser
+
 import bs4
+import os
 import requests
 import sys
 
-SEARCHED_WORDS = []
+SOURCES = "vocabularies"
+VOCABULARY_FILE = os.path.join(SOURCES, "searched_words.txt")
+SEARCHED_WORDS = {}
 
 class Word:
     def __init__(self, word):
         self.word = word
+        self._read_searched_words()
 
     def __str__(self):
         return f"{self.word}"
 
+    def _read_searched_words(self):
+        with open(VOCABULARY_FILE) as already_searched:
+            searched_words = already_searched.readlines()
+         
+            for line in searched_words:
+                if line != '':
+                    word, article = line.split('\t')
+                    SEARCHED_WORDS[word] = article.strip('\n')
+            print(SEARCHED_WORDS)
+                
     def gender(self):
+        if self.word in SEARCHED_WORDS.keys():
+            return SEARCHED_WORDS[self.word]
+        else:
+            return self._look_in_www() 
+
+    def _look_in_www(self):
         # look up gender in www.welklidwoord.nl
         try:
             url_gender_guesser = 'https://www.welklidwoord.nl/' + self.word
@@ -20,34 +41,31 @@ class Word:
             
             if gender_guesser_page.status_code == requests.codes.ok:
                 beautifulsoup_of_genderpage = bs4.BeautifulSoup(gender_guesser_page.text, features='lxml')
-                gender = beautifulsoup_of_genderpage.select('h1 span')[0].getText()
+                gender = beautifulsoup_of_genderpage.select('h2 span')[0].getText()
 
                 if len(gender) > 3:
                     return ""
                 else:
+                    SEARCHED_WORDS[self.word] = gender.lower()
                     return gender.lower()             
         except(IndexError):
             print("Please, write a complete word")
 
-def keep_track_of_search(gender, word):
-    SEARCHED_WORDS.append((gender, word))    
 
 def keep_search_history():
-    with open("searched_words.txt",'a') as output_file:
-        for gender, word  in SEARCHED_WORDS: 
-            output_file.write(gender + "\t" + word + "\n")
+    with open(VOCABULARY_FILE,'w') as output_file:
+        for word in SEARCHED_WORDS.keys(): 
+            output_file.write(word + '\t' + SEARCHED_WORDS[word] + '\n')
     
 while(True):
     try:
-        word = input("Welk woord zoek je? >>> ")
+        word = input("Which word do you want to look up? >>> ")
         w = Word(word)
         g = w.gender()
         if g:
-            print(f"Het lidwoord van {word} is \'{w.gender()}\'")
-            # adds tuple to SEARCHED_WORDS
-            keep_track_of_search(g, word)
+            print(f"The grammatical gender of {word} is '{g}'")
         else:
-            print(f"Ik weet niet welk lidwoord wordt gebruikt met {w}")
+            print(f"I don't know which article belongs to {w}")
     except(KeyboardInterrupt):
         # all searched words are written in once
         keep_search_history()
